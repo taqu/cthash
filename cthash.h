dedeeea37d7acfe5137e41bef253b8a138124277
taqu <taqu2920.dev@gmail.com>
2025-10-15 03:53:53 +0900

    Remove unused functions

--- cthash.h
+++ cthash.h
@@ -1,5 +1,56 @@
 ﻿#ifndef INC_CRYPT_H_
 #define INC_CRYPT_H_
+/*
+# License
+This software is distributed under two licenses, choose whichever you like.
+
+## MIT License
+Copyright (c) 2025 Takuro Sakai
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Public Domain
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
+*/
 #include <cstdint>
 #include <array>
 #ifdef _DEBUG
@@ -7,7 +58,7 @@
 #endif
 #include <bit>
 
-namespace crypt
+namespace cthash
 {
 
     struct sha256_state
@@ -139,12 +190,12 @@ namespace crypt
 
         constexpr std::array<uint8_t, 32> finalize()
         {
-            uint64_t i = length_;
+            uint64_t l = length_;
             uint8_t end = length_ < 56 ? 56 : 64;
 
-            data_[i++] = 0x80; // Append a bit 1
-            while (i < end) {
-                data_[i++] = 0x00; // Pad with zeros
+            data_[l++] = 0x80; // Append a bit 1
+            while (l < end) {
+                data_[l++] = 0x00U; // Pad with zeros
             }
 
             if (56 <= length_) {
@@ -358,69 +409,6 @@ namespace crypt
         std::copy(src, src + N, message.begin());
         chacha20_encrypt(input, N, &message[0]);
         return message;
-    }
-
-    template <class T, std::size_t N>
-    struct wrapper_list;
-
-    template <std::size_t N>
-    struct wrapper_list<uint8_t, N> final
-    {
-        constexpr wrapper_list() noexcept
-            : data_{}
-        {
-        }
-        constexpr wrapper_list(const std::array<uint8_t, N>& data) noexcept
-            : data_(data)
-        {
-        }
-
-        constexpr uint8_t operator[](std::size_t i) const noexcept
-        {
-            return data_[i];
-        }
-        constexpr uint8_t& operator[](std::size_t i) noexcept
-        {
-            return data_[i];
-        }
-
-        constexpr size_t size() const noexcept { return data_.size(); }
-
-        std::array<uint8_t, N> data_;
-    };
-
-    template <class T, std::size_t N>
-    std::ostream& operator<<(std::ostream& os, const wrapper_list<T, N>& ar)
-    {
-        for (size_t i = 0; i < ar.size(); ++i)
-        {
-            os << std::hex << (int32_t)ar[i];
-        }
-        return os;
-    }
-
-    template <std::size_t N>
-    constexpr auto encrypt(const char(&input)[N])
-    {
-        constexpr auto SIZE = N - 1;
-        std::array<uint8_t, SIZE> result;
-        for (std::size_t i = 0; i < SIZE; ++i)
-        {
-            result[i] = static_cast<uint8_t>(input[i]) + 1;
-        }
-        return wrapper_list<uint8_t, SIZE>(result);
-    }
-
-    template <std::size_t N>
-    constexpr wrapper_list<uint8_t, N> decrypt(const wrapper_list<uint8_t, N>& encrypted)
-    {
-        wrapper_list<uint8_t, N> result;
-        for (std::size_t i = 0; i < result.size(); ++i)
-        {
-            // -1もどす
-            result[i] = static_cast<char>(encrypted[i] - 1);
-        }
-        return result;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 ﻿#include <iostream>
 #include "cthash.h"
 
-static constexpr std::array<uint8_t, 32> key = crypt::sha256_string((const uint8_t[])"Hello World!");
-static constexpr auto encrypted = crypt::chacha20_encrypt(key, (const uint8_t[])"Hello World!");
+static constexpr std::array<uint8_t, 32> key = cthash::sha256_string((const uint8_t[])"Hello World!");
+static constexpr auto encrypted = cthash::chacha20_encrypt(key, (const uint8_t[])"Hello World!");
 
 int main(void)
 {
     std::cout << key << std::endl;
     std::cout << "encrypted: " << encrypted << std::endl;
-    auto decrypted = crypt::chacha20_encrypt(key, encrypted);
+    auto decrypted = cthash::chacha20_encrypt(key, encrypted);
     std::cout << "decrypted: " << decrypted << std::endl;
     return 0;
 }
